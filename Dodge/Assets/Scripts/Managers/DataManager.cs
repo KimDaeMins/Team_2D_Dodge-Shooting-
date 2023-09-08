1c34d74a25eb3a1421d44269b0eb97c4a362a634
KimDaeMins <eoals81712@gmail.com>
2023-09-08 15:29:13 +0900

    찐막

--- Dodge/Assets/Scripts/Managers/DataManager.cs
+++ Dodge/Assets/Scripts/Managers/DataManager.cs
@@ -10,33 +10,41 @@ public class DataManager
     public Sprite UserSprite { get; set; }
     public RuntimeAnimatorController AnimatorController { get; set; }
 
-    public Dictionary<string , Item> items = new Dictionary<string , Item>();
+    public Dictionary<string, Item> _itemsDic = new Dictionary<string , Item>();
+    public List<Item> _itemslist = new List<Item>();
     public void Init()
     {
-        //UserSprite = Managers.Resource.LoadSprite("elf");
+        //여기서 딕셔너리랑 리스트에 아이템을 전부 넣어준다
     }
 
-    public bool UseItem(string name)
+    public bool UseItem(int index)
     {
-        if (items.ContainsKey(name) == false)
-        {
-            return false;
-        }
-        if (items[name].Count == 0)
+        if (_itemslist[index].Count == 0)
         {
             return false;
         }
 
-        items[name].Count -= 1;
+        _itemslist[index].Count -= 1;
 
         return true;
     }
 
-    public void GetItem(string name)
+    public bool GetItem(int index)
     {
-        Item item = items[name];
+        if (_itemslist[index].Count == _itemslist[index].MaxCount)
+            return false;
+
+        _itemslist[index].Count++;
+        return true;
     }
-    public void GetItem(Item item)
+    public bool GetItem(Item item)
     {
+        Item it = _itemsDic[item.Name];
+
+        if (it.Count == it.MaxCount)
+            return false;
+
+        it.Count = Math.Min(it.MaxCount , it.Count + item.Count);
+        return true;
     }
 }
