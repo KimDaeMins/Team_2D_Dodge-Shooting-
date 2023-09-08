ebf2a4e38fc2da625abe79b78a76c1f560cd75c5
rlaalsxo <alsxo124@naver.com>
2023-09-08 20:26:31 +0900

    PlayerFire

--- Dodge/Assets/Scripts/Objects/Player/Player.cs
+++ Dodge/Assets/Scripts/Objects/Player/Player.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Object_Base
+public class Player : Object_Base, IFire
 {
     public int Hp { get; set; }
     [SerializeField] protected Animator _animator;
+    protected Camera _camera;
+    [SerializeField] protected Rigidbody2D _rb2d;
+    public float FireDelay { get; set; }
+    public float FireCoolTime { get; set; }
+    public void FireUpdate()
+    {
+        if (FireCoolTime > FireDelay)
+            FireDelay += Time.deltaTime;
+    }
+    public bool FireCheck()
+    {
+        if (FireDelay > FireCoolTime)
+            return true;
+        return false;
+    }
+
+    public void Fire()
+    {
+        if (FireCheck())
+        {
+            FireDelay = 0;
+            Debug.Log("Fire");
+        }
+        else
+        {
+            Debug.Log("CoolTime");
+        }
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("충돌");
