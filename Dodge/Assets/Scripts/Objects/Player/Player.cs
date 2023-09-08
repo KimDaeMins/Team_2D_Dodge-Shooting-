2fb1823f6db2410cf908c8d99faad29823104fc1
rlaalsxo <alsxo124@naver.com>
2023-09-08 22:29:49 +0900

    PlayerBone

--- Dodge/Assets/Scripts/Objects/Player/Player.cs
+++ Dodge/Assets/Scripts/Objects/Player/Player.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Player : Object_Base, IFire
 {
-    public int Hp { get; set; }
-    [SerializeField] protected Animator _animator;
-    protected Camera _camera;
-    [SerializeField] protected Rigidbody2D _rb2d;
+    public int Hp;
+    public int Atk;
     public float FireDelay { get; set; }
     public float FireCoolTime { get; set; }
+    public float Speed { get { return _speed; } set { _speed = value; } }
+    [SerializeField] public Animator _animator;
+    [SerializeField] public Rigidbody2D _rb2d;
+    public Camera _camera;
+    private void Awake()
+    {
+        //_camera = Camera.main;
+        Hp = 3;
+        Atk = 1;
+        FireDelay = 0;
+        FireCoolTime = 0.4f;
+    }
+    private void Update()
+    {
+        FireUpdate();
+    }
     public void FireUpdate()
     {
         if (FireCoolTime > FireDelay)
@@ -34,9 +48,21 @@ public class Player : Object_Base, IFire
             Debug.Log("CoolTime");
         }
     }
+    void GetDamage(int damage)
+    {
+        Hp -= damage;
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("충돌");
-        _animator.SetTrigger("Hit");
+        if(collision.tag != "Item")
+        {
+            Debug.Log("충돌");
+            GetDamage(1);
+            _animator.SetTrigger("Hit");
+        }
+        else
+        {
+            Managers.Data.GetItem(0);
+        }
     }
 }
