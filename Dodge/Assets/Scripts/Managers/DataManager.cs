da920fb53eafe65cfce3bf7b8c5d60595ed225fc
KimDaeMins <eoals81712@gmail.com>
2023-09-08 16:42:48 +0900

    다섯번째

--- Dodge/Assets/Scripts/Managers/DataManager.cs
+++ Dodge/Assets/Scripts/Managers/DataManager.cs
@@ -12,9 +12,15 @@ public class DataManager
 
     public Dictionary<string, Item> _itemsDic = new Dictionary<string , Item>();
     public List<Item> _itemslist = new List<Item>();
+
     public void Init()
     {
         //여기서 딕셔너리랑 리스트에 아이템을 전부 넣어준다
+        //Item item = new Item();
+        //item.InitItem("이름" , 최대갯수);
+        //item.Count = 0;
+        //_itemsDic.Add(item.name , item);
+        //_itemslist.Add(item);
     }
 
     public bool UseItem(int index)
