1edb7adfb04e73962a09785b76a1f2ffdfe31624
jwonp9127 <wldnjs9127@naver.com>
2023-09-11 10:21:07 +0900

    {Add] Moster

--- Dodge/Assets/Scripts/Objects/Monster.cs
+++ Dodge/Assets/Scripts/Objects/Monster.cs
@@ -1,8 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Monster : Object_Base
 {
-    public int Hp { get; set; }
+    public int CurrentHp { get; set; }
+    public float MoveSpeed { get; set; }
+    public Vector2 MoveDirection { get; set; }
+
+    private Rigidbody2D _rigidbody;
+    public Animator animator;
+
+    protected void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected void FixedUpdate()
+    {
+        Move(MoveDirection);
+    }
+
+
+    protected void GetDamage(int damage)
+    {
+        CurrentHp = 0;
+        Destroy(this);
+    }
+
+    protected void Move(Vector2 direction)
+    {
+        direction *= _speed;
+        _rigidbody.velocity = direction;
+    }
+
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PlayerBullet"))
+        {
+            GetDamage(100);
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(this);
+        }
+    }
 }
+
+
