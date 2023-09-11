a3ccc0684a63f3272544a2d3de3679b0b618f3a2
AYJ12 <95719027+AYJ12@users.noreply.github.com>
2023-09-11 22:07:26 +0900

    예외처리 오류 수정

--- Dodge/Assets/Scripts/Objects/PlayerGudiedBullet.cs
+++ Dodge/Assets/Scripts/Objects/PlayerGudiedBullet.cs
@@ -31,13 +31,15 @@ public class PlayerGudiedBullet : Object_Base, IBullet
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();   //총알 움직임 위해
-        _target = GameObject.FindWithTag("Monster"); //타겟 탐색
-        _targetVector = (_target.transform.position - transform.position).normalized;   //타겟 방향 단위벡터
+        if ((_target = GameObject.FindWithTag("Monster")) != null) //타겟 탐색
+            _targetVector = (_target.transform.position - transform.position).normalized;   //타겟 방향 단위벡터
+        else
+            _targetVector = transform.up;
     }
 
     public void Move()
     {
-        _rigidBody.velocity = (_targetVector * _speed) * Time.deltaTime;
+        _rigidBody.velocity = _targetVector * _speed * Time.deltaTime;
     }
 
     public bool DeadCheck()
@@ -64,36 +66,39 @@ public class PlayerGudiedBullet : Object_Base, IBullet
         {
             _hitEffect = Managers.Resource.Instantiate("MonsterHitEffect", transform.position);
             Managers.Resource.Destroy(_hitEffect, 0.5f);
-            other.GetComponent<Monster>().GetDamage(_damage);
+            other.GetComponent<Player>().GetDamage(_damage);
             Managers.Resource.Destroy(this.gameObject);
         }
     }
 
     private void Follow()
     {
-        if (gameObject.activeSelf && _lifeTime > 5) //총알이 살아있고 얼마나 가까운지 조건 임의로 5초 조건 넣어둠
+        if (_target != null)
         {
-            _targetVector = (_target.transform.position - transform.position).normalized;
-            // 내적(dot)을 통해 각도를 구함
-            float dot = Vector3.Dot(transform.up, _targetVector);
-            if (dot < 1.0f)
+            if (gameObject.activeSelf && _lifeTime > 5) //총알이 살아있고 얼마나 가까운지 조건 임의로 5초 조건 넣어둠
             {
-                float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
-
-                // 외적을 통해 각도의 방향을 판별
-                Vector3 cross = Vector3.Cross(transform.up, _targetVector).normalized;
-                // 외적 결과 값에 따라 각도 반영
-                if (cross.z < 0)
+                _targetVector = (_target.transform.position - transform.position).normalized;
+                // 내적(dot)을 통해 각도를 구함
+                float dot = Vector3.Dot(transform.up, _targetVector);
+                if (dot < 1.0f)
                 {
-                    angle = transform.rotation.eulerAngles.z - Mathf.Min(10, angle);
-                }
-                else
-                {
-                    angle = transform.rotation.eulerAngles.z + Mathf.Min(10, angle);
-                }
+                    float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
 
-                transform.rotation = Quaternion.Euler(0, 0, angle).normalized;
-                // angle이 윗 방향과 target의 각도.
+                    // 외적을 통해 각도의 방향을 판별
+                    Vector3 cross = Vector3.Cross(transform.up, _targetVector).normalized;
+                    // 외적 결과 값에 따라 각도 반영
+                    if (cross.z < 0)
+                    {
+                        angle = transform.rotation.eulerAngles.z - Mathf.Min(10, angle);
+                    }
+                    else
+                    {
+                        angle = transform.rotation.eulerAngles.z + Mathf.Min(10, angle);
+                    }
+
+                    transform.rotation = Quaternion.Euler(0, 0, angle).normalized;
+                    // angle이 윗 방향과 target의 각도.
+                }
             }
         }
     }
@@ -108,5 +113,5 @@ public class PlayerGudiedBullet : Object_Base, IBullet
             Managers.Resource.Destroy(this.gameObject);
         }
     }
-   
+
 }
