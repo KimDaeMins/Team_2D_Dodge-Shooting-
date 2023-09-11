64cd4bd79015cfe84d87ec5938ea395b0b4995a2
AYJ12 <95719027+AYJ12@users.noreply.github.com>
2023-09-11 15:40:32 +0900

    BulletPrefabs

--- Dodge/Assets/Scripts/Objects/MonsterGudiedBullet.cs
+++ Dodge/Assets/Scripts/Objects/MonsterGudiedBullet.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MonsterGudiedBullet : Object_Base, IBullet
 {
-    public Player _player;
     private Rigidbody2D _rigidBody;
     private int _damage = 5; // 총알 데미지
     private float _lifeTime = 10.0f; //총알이 살아있는 시간
