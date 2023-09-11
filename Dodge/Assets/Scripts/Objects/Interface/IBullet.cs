c9bba9a1920b9c8566a3dfd76d7e46664d8fb3f1
AYJ12 <95719027+AYJ12@users.noreply.github.com>
2023-09-11 10:19:49 +0900

    simple bullet 커밋

--- Dodge/Assets/Scripts/Objects/Interface/IBullet.cs
+++ Dodge/Assets/Scripts/Objects/Interface/IBullet.cs
@@ -7,9 +7,7 @@ public interface IBullet
     public float Damage { get; set; }
     public GameObject Target { get; set; }
     public float LifeTime { get; set; }
-    public Vector2 Dir { get; set; }
 
-    public Transform GetTransform();
 
     public void Move();
 
