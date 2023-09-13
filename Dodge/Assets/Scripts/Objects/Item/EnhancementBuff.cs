885a1acf14a26969a33b0b2d489a0472cf23686d
hibernate95 <trytostart95@gmail.com>
2023-09-13 17:02:28 +0900

    폭탄스프라이트 추가

--- Dodge/Assets/Scripts/Objects/Item/EnhancementBuff.cs
+++ Dodge/Assets/Scripts/Objects/Item/EnhancementBuff.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnhancementBuff : Inven_Base
 {
+    
+    
+
     public override void UseItem()
     {
-
-
-        Debug.Log("10초간 강력한 공격을 발사합니다.");
-
+        
     }
+
+
 }
--- Dodge/Assets/Scripts/Objects/Item/JammingBomb.cs
+++ Dodge/Assets/Scripts/Objects/Item/JammingBomb.cs
@@ -9,7 +9,22 @@ public class JammingBomb : Inven_Base
 
         Managers.Resource.Instantiate("JammingPrefab");
 
-        Debug.Log("투사체를 국소적으로 파괴합니다.");
 
+        //LinkedList<GameObject> monsterBullets = Managers.Object.GetAllObject(Define.Object.MonsterBullet);
+        //void OnTriggerEnter2D(Collider2D other)
+        //{
+        //    if (other.CompareTag("monsterBullet"))
+        //    {
+        //        Player player = other.GetComponent<Player>();
+
+
+
+        //        // 아이템을 화면에서 제거하거나 기타 처리를 할 수 있습니다.
+        //        Managers.Resource.Destroy(monsterBullets);
+        //        Debug.Log("투사체를 국소적으로 파괴합니다.");
+
+        //    }
+        //}
+        
     }
 }
