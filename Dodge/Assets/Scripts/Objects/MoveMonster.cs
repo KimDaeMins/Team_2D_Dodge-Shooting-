8bdeb60d8df0e8b12a3c8836107b3571904782f3
rlaalsxo <alsxo124@naver.com>
2023-09-13 12:01:00 +0900

    MoveMonsterUpdate

--- Dodge/Assets/Scripts/Objects/MoveMonster.cs
+++ Dodge/Assets/Scripts/Objects/MoveMonster.cs
@@ -4,22 +4,28 @@ using System.ComponentModel.Design;
 using Unity.VisualScripting;
 using UnityEngine;
 /// <summary>
-/// 왼쪽으로 이동하며 정면으로 공격하는 몬스터, 이동 후 일정이상 스크린 밖으로 벗어나면 사라짐
+/// 오른쪽으로 이동하며 정면으로 공격하는 몬스터, 이동 후 일정이상 스크린 밖으로 벗어나면 사라짐
 /// </summary>
-public class LeftMoveMonster : Monster, IFire
+public class MoveMonster : Monster, IFire
 {
     public float FireCoolTime { get; set; }
     public bool IsFireAble { get; set; }
-    
+    [SerializeField] Vector3 _move;
+    [SerializeField] int _bulletCount;
+    [SerializeField] string _bullet;
+
     protected override void Awake()
     {
         base.Awake();
-        _currentHp = 1;
         FireCoolTime = 1f;
         IsFireAble = true;
         _damage = 1;
+        _move = _move.normalized;
     }
-
+    private void OnEnable()
+    {
+        _currentHp = 1;
+    }
     protected override void Update()
     {
         base.Update();
@@ -28,7 +34,7 @@ public class LeftMoveMonster : Monster, IFire
 
     protected override void MoveDirectionUpdate()
     {
-        _moveDirection = new Vector2(-1 * transform.right.x, transform.right.y) * (_speed * Time.deltaTime);
+        _moveDirection = this.transform.rotation * _move * (_speed * Time.deltaTime);
     }
     
     public IEnumerator FireUpdate(float coolTime)
@@ -41,10 +47,12 @@ public class LeftMoveMonster : Monster, IFire
     {
         if (IsFireAble)
         {
-            Managers.Resource.Instantiate("MonsterBullet" , transform.position, transform.rotation);
-            
-            StartCoroutine("FireUpdate", FireCoolTime);
+            for (int i = 0; i < _bulletCount; i++)
+            {
+                Managers.Resource.Instantiate(_bullet, transform.position, transform.rotation);
+            }
             IsFireAble = false;
+            StartCoroutine("FireUpdate", FireCoolTime);
         }
     }
 }   
