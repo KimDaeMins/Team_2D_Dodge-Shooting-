7f5897e6d1efe93544bae00185abbd2856412b01
jwonp9127 <wldnjs9127@naver.com>
2023-09-13 15:57:44 +0900

    Radial Monster 추가

---
+++ Dodge/Assets/Scripts/Objects/BossMonster.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using Unity.VisualScripting;
+using UnityEngine;
+/// <summary>
+/// 제자리에서 정면으로 공격하는 몬스터, 여러 총알 동시에 사용
+/// </summary>
+public class BossMonster : Monster, IFire
+{
+    public float FireCoolTime { get; set; }
+    public bool IsFireAble { get; set; }
+    [SerializeField] private bool _move;
+    private int _pattern;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        _currentHp = 1;
+        FireCoolTime = 1f;
+        IsFireAble = true;
+        _damage = 1;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        Fire();
+        _pattern = Random.Range(1, 7);
+    }
+
+    protected override void FixedUpdate()
+    {
+        if (_move)
+        {
+            base.FixedUpdate();
+        }
+    }
+    
+    public IEnumerator FireUpdate(float coolTime)
+    {
+        yield return new WaitForSeconds(coolTime);
+        IsFireAble = true;
+    }
+
+    public virtual void Fire()
+    {
+        if (IsFireAble)
+        {
+            var _rot = transform.eulerAngles;
+            Managers.Resource.Instantiate("MonsterBullet", transform.position,
+                Quaternion.Euler(_rot.x, _rot.y, _rot.z - 30f));
+            Managers.Resource.Instantiate("MonsterBullet", transform.position,
+                Quaternion.Euler(_rot.x, _rot.y, _rot.z - 20f));
+            Managers.Resource.Instantiate("MonsterBullet", transform.position,
+                Quaternion.Euler(_rot.x, _rot.y, _rot.z - 10f));
+            Managers.Resource.Instantiate("MonsterBullet" , transform.position, _rot);
+            Managers.Resource.Instantiate("MonsterBullet", transform.position,
+                Quaternion.Euler(_rot.x, _rot.y, _rot.z + 10f));
+            Managers.Resource.Instantiate("MonsterBullet", transform.position,
+                Quaternion.Euler(_rot.x, _rot.y, _rot.z + 20f));
+            Managers.Resource.Instantiate("MonsterBullet", transform.position,
+                Quaternion.Euler(_rot.x, _rot.y, _rot.z + 30f));
+
+            StartCoroutine("FireUpdate", FireCoolTime);
+            IsFireAble = false;
+        }
+    }
+    
+    private void OnEnable()
+    {
+        _currentHp = 1;
+    }
+}   
+
